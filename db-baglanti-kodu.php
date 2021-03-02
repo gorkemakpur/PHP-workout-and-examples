e26ac65b6ade0be636e6b0287ffcc0727ae98a25
gorkemakpur <61268434+gorkemakpur@users.noreply.github.com>
2021-03-02 14:08:54 +0300

    .
    .

---
+++ db-baglanti-kodu.php
@@ -0,0 +1,10 @@
+<?php
+
+try {
+$db=new PDO("mysql:host=localhost;dbname=udemy;charset=utf8",'root','');
+echo "Veritabanı Bağlantısı Başarılı!";
+}
+catch (PDOException $e){
+    echo $e->getMessage();    
+}
+?>
